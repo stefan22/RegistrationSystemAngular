92e3fb4bbb4f5701fe2d5dad10714346171f2542
Stefan Lissia <stefan.lissia@vml.com>
2016-07-19 12:52:05 +0100

    registration form validation and register function

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -2,6 +2,11 @@ myApp.controller('RegistrationController', ['$scope', function($scope) {
 
 	$scope.login = function() {
 		$scope.message = "Welcome " + $scope.user.email;
-	}
+	};
+
+	$scope.register = function() {
+		$scope.message = "Now you can access site features " + $scope.user.firstname;
+	};
+
 
 }]);
